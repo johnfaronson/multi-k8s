80ebcff426e850bd5016bf64f5f21c3228493e2c
John Aronson <john.aronson@appdynamics.com>
2019-05-07 16:52:01 -0600

    updated docker user

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t johnfaronson/multi-client:latest -t johnfaronson/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t johnfaronson/multi-server:latest -t johnfaronson/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t johnfaronson/multi-worker:latest -t johnfaronson/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push johnfaronson/multi-client:latest
-docker push johnfaronson/multi-server:latest 
-docker push johnfaronson/multi-worker:latest
-docker push johnfaronson/multi-client:$SHA
-docker push johnfaronson/multi-server:$SHA
-docker push johnfaronson/multi-worker:$SHA
+docker build -t jfaronson/multi-client:latest -t jfaronson/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jfaronson/multi-server:latest -t jfaronson/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jfaronson/multi-worker:latest -t jfaronson/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push jfaronson/multi-client:latest
+docker push jfaronson/multi-server:latest 
+docker push jfaronson/multi-worker:latest
+docker push jfaronson/multi-client:$SHA
+docker push jfaronson/multi-server:$SHA
+docker push jfaronson/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/client-deployment client=johnfaronson/multi-client:$SHA
-kubectl set image deployments/server-deployment server=johnfaronson/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=johnfaronson/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=jfaronson/multi-client:$SHA
+kubectl set image deployments/server-deployment server=jfaronson/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=jfaronson/multi-worker:$SHA
